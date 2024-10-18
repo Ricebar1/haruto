8c23043fa5ae4d732dc0ccff388aa717814b064d
Ricebar1 <t236087@st.yamagata-u.ac.jp>
2024-10-18 19:26:51 +0900

    Add files via upload

---
+++ hukunokazu.c
@@ -0,0 +1,17 @@
+int main()
+{
+    int A, B;
+    
+    printf("あなたの夏服の数は？");
+    scanf("%d",&A);
+    
+    printf("あなたの冬服の数は？");
+    scanf("%d",&B);
+    
+     int sum=A+B;
+    
+    printf("あなたの持っている服の数は%dです。\n",sum);
+    
+    return 0;
+    
+}
