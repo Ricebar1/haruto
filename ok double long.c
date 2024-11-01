c30c933c81905c39ba7dd75110e78867ce7a377b
Ricebar1 <t236087@st.yamagata-u.ac.jp>
2024-11-01 19:28:35 +0900

    Add files via upload

---
+++ ok double long.c
@@ -0,0 +1,12 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("long doubleの最大値:%Lf\n",LDBL_MAX);
+    printf("long doubleの最小値:%Lf\n",LDBL_MIN);
+    printf("long doubleの精度:%Lf\n",LDBL_DIG);
+    
+    return 0;
+}
